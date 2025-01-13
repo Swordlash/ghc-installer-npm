0814315055cc6277d041e5b0d5415144ad1aa3a4
Mateusz Goślinowski <mateusz.goslinowski@gmail.com>
2025-01-13 13:54:13 +0100

    Implement ghcup-npm

---
+++ index.js
@@ -0,0 +1,24 @@
+#!/usr/bin/env node
+
+const { program } = require('commander');
+const { version } = require('./package.json');
+const { install, run } = require('./ghcup');
+
+program
+  .name('ghcup-npm')
+  .description('An NPM wrapper for managing GHC, Cabal, and Stack via ghcup')
+  .version(version);
+
+program
+  .command('install <component> [version]')
+  .description('Install & set a GHC, Cabal, or Stack version')
+  .action(install);
+
+program
+  .command('run <command> [args...]')
+  .description('Run installed command (e.g., ghc, cabal, stack)')
+  .allowUnknownOption(true)
+  .action(run);
+
+program.showHelpAfterError();
+program.parse(process.argv);
